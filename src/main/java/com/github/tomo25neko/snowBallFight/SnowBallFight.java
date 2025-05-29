00975b06aadc52dd27ff3ccdba1dbdaecc633654
tomo25 <tomotomo2525neko@gmail.com>
2025-05-29 21:49:21 +0900

    初期構築

--- src/main/java/com/github/tomo25neko/snowBallFight/SnowBallFight.java
+++ src/main/java/com/github/tomo25neko/snowBallFight/SnowBallFight.java
@@ -3,8 +3,20 @@ package com.github.tomo25neko.snowBallFight;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariConfig;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public final class SnowBallFight extends JavaPlugin {
+
     private static Plugin plugin;
+
+    String jdbcUrl = "jdbc:h2:./plugins/snowball-fight/data/db;MODE=MySQL;AUTO_SERVER=TRUE";
+    String username = "sa";   // デフォルトユーザー
+    String password = "";     // デフォルトは空パスワード
 
     @Override
     public void onEnable() {
@@ -21,7 +33,16 @@ public final class SnowBallFight extends JavaPlugin {
         getLogger().info("SnowBall-Fightが停止しました!");
     }
 
-    public static Plugin MyInstance() {
+    public static Plugin myInstance() {
         return plugin;
+    }
+
+    private void prepareDataFolder() {
+        File dataFolder = new File(getDataFolder(), "SnowBall-Fight");
+        if (!dataFolder.exists()) {
+            if (!dataFolder.mkdirs()) {
+                getLogger().warning("データフォルダの作成に失敗しました: " + dataFolder.getAbsolutePath());
+            }
+        }
     }
 }
